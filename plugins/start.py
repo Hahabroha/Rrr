23d7c7033af38e60c4549e520c23267604a19468
Hassanaliuuq <106004498+Hassanaliuuq@users.noreply.github.com>
2023-11-06 14:17:54 +0500

    Update start.py

--- plugins/start.py
+++ plugins/start.py
@@ -2,13 +2,45 @@ from asyncio import sleep
 from pyrogram import Client, filters
 from pyrogram.types import InlineKeyboardButton, InlineKeyboardMarkup, ForceReply, CallbackQuery
 from pyrogram.errors import FloodWait
+from pyshorteners import Shortener
+import redis
+from pyrogram import Client
+from pyrogram.types import InlineKeyboardButton, InlineKeyboardMarkup
 import humanize
 import random
 from helper.txt import mr
 from helper.database import db
 from config import START_PIC, FLOOD, ADMIN 
 
+Initialize Redis client
+r = redis.StrictRedis(host='localhost', port=6379, db=0)
 
+@Client.on_message(filters.private & filters.command(["start"]))
+async def start(client, message):
+    user = message.from_user
+    user_id = user.id
+
+    if not r.get(f'verified:{user_id}'):
+        # Generate a random verification code
+        verification_code = random.randint(1000, 9999)
+
+        # Generate a random shortened URL
+        s = Shortener()
+        full_url = "https://zxlink.in/jmezY1"  # Replace with the actual URL you want users to complete
+        shortened_url = s.tinyurl.short(full_url)
+
+        # Store the verification code and shortened URL in the database
+        r.setex(f'verification_code:{user_id}', 600, verification_code)
+        r.set(f'shortened_url:{user_id}', shortened_url)
+
+        # Send the user a verification message with the shortened URL
+        verification_message = (
+            f"Please complete the following URL: {shortened_url}\n"
+            f"Verification Code: {verification_code}"
+        )
+        await message.reply_text(verification_message)
+    else:
+        # User is verified, proceed with your code
 @Client.on_message(filters.private & filters.command(["start"]))
 async def start(client, message):
     user = message.from_user
